13a4eecea3915629cdd8644e27fb844ad8574f83
Glitch (6933-pit-scouter) <none>
2020-02-27 02:59:41 +0000

    🕶🏙 Updated with Glitch

--- www/js/client.js
+++ www/js/client.js
@@ -21,7 +21,139 @@ var scoutNames = [
   { name: "Paige" },
   { name: "Ben Weatherill" },
   { name: "Elliot" },
-  { name: "... GUEST ..."}
+  { name: "... GUEST ..." }
+];
+
+var teamNames = [
+  { num: 95, name: "Grasshoppers", insta_link: null },
+  {
+    num: 133,
+    name: "B.E.R.T.",
+    insta_link: "https://www.instagram.com/frcbert133/"
+  },
+  {
+    num: 151,
+    name: "Tough Techs",
+    insta_link: "https://www.instagram.com/toughtechs151/"
+  },
+  { num: 190, name: "Team 190", insta_link: "" },
+  { num: 254, name: "Cheesy Poofs", insta_link: "" },
+  {
+    num: 319,
+    name: "Big Bad Bob",
+    insta_link: "https://www.instagram.com/bigbadbob319/"
+  },
+  {
+    num: 467,
+    name: "The Colonials",
+    insta_link: "https://www.instagram.com/shrewsburyrobotics467/"
+  },
+  {
+    num: 501,
+    name: "Power Knights",
+    insta_link: "https://www.instagram.com/frcteam501/"
+  },
+  { num: 663, name: "Robonauts", insta_link: "" },
+  { num: 811, name: "Cardinals", insta_link: "" },
+  { num: 971, name: "Spartan Robotics", insta_link: "" },
+  {
+    num: 1058,
+    name: "PVC Pirates",
+    insta_link: "https://www.instagram.com/frcteam1058/"
+  },
+  {
+    num: 1307,
+    name: "Robosaints",
+    insta_link: "https://www.instagram.com/1307robosaints/"
+  },
+  { num: 1512, name: "Big Red", insta_link: "" },
+  { num: 1517, name: "AMP'D UP", insta_link: "" },
+  { num: 1519, name: "Mechanical Mayhem", insta_link: "" },
+  {
+    num: 1729,
+    name: "Inconceivable",
+    insta_link: "https://www.instagram.com/team_1729/"
+  },
+  {
+    num: 1831,
+    name: "Screaming Eagles",
+    insta_link: "https://www.instagram.com/frc_1831/"
+  },
+  {
+    num: 3566,
+    name: "Gone Fishing",
+    insta_link: "https://www.instagram.com/robotics3566/"
+  },
+  {
+    num: 3609,
+    name: "Duct Tape Dragons",
+    insta_link: "https://www.instagram.com/frc3609/"
+  },
+  {
+    num: 4761,
+    name: "Roborockets",
+    insta_link: "https://www.instagram.com/robockets.4761/"
+  },
+  {
+    num: 4905,
+    name: "Andromeda One",
+    insta_link: "https://www.instagram.com/frc4905/"
+  },
+  {
+    num: 4909,
+    name: "Bionics",
+    insta_link: "https://www.instagram.com/team4909/"
+  },
+  {
+    num: 4929,
+    name: "Maroon Monsoon",
+    insta_link: "https://www.instagram.com/frcteam4929/"
+  },
+  {
+    num: 5633,
+    name: "Hyde Mecha Wolves",
+    insta_link: "https://www.instagram.com/hyde.robotics/"
+  },
+  {
+    num: 5687,
+    name: "Outliers",
+    insta_link: "https://www.instagram.com/frc5687/"
+  },
+  {
+    num: 5902,
+    name: "The Wire Clippers",
+    insta_link: "https://www.instagram.com/frc5902/"
+  },
+  {
+    num: 6153,
+    name: "The Blue Crew",
+    insta_link: "https://www.instagram.com/bluecrewrobotics/"
+  },
+  {
+    num: 6161,
+    name: "Equilibrium",
+    insta_link: "https://www.instagram.com/team_6161/"
+  },
+  { num: 6691, name: "Torque", insta_link: "" },
+  {
+    num: 6763,
+    name: "FUSION",
+    insta_link: "https://www.instagram.com/fusion6763/"
+  },
+  {
+    num: 6933,
+    name: "Archytas",
+    insta_link: "https://www.instagram.com/archytasrobotics/"
+  },
+  { num: 7133, name: "Steam Makers", insta_link: "" },
+  {
+    num: 7416,
+    name: "Northern Horizons",
+    insta_link: "https://www.instagram.com/northernhorizons7416/"
+  },
+  { num: 7913, name: "'Bear'ly Functioning", insta_link: "" },
+  { num: 8046, name: "Laker Bots", insta_link: "" },
+  { num: 99999, name: "... OTHER ... (mention in notes)", insta_link: "" }
 ];
 
 $(document).ready(() => {
@@ -32,45 +164,46 @@ $(document).ready(() => {
     .addClass("d-none")
     .removeClass("d-block");
 
-// START OFFLINE DATA ACCESS
-loadLocalScouts();  
-// END OFFLINE DATA ACCESS
-  
-// // START ONLINE DATA ACCESS
-//   $.ajax({
-//     url: "/team/",
-//     type: "GET",
-//     dataType: "json",
-//     success: data => {
-//       console.log("You received some data!", data);
-//       $.each(data, function(i, item) {
-//         $("#numBox").append(
-//           $("<option>", {
-//             value: item.num,
-//             text: item.num + ": " + item.name
-//           })
-//         );
-//       });
-//     }
-//   });
+  // START OFFLINE DATA ACCESS
+  loadLocalScouts();
+  loadLocalTeams();
+  // END OFFLINE DATA ACCESS
 
-//   $.ajax({
-//     url: "/user/",
-//     type: "GET",
-//     dataType: "json",
-//     success: data => {
-//       console.log("You received some data!", data);
-//       $.each(data, function(i, item) {
-//         $("#nameBox").append(
-//           $("<option>", {
-//             value: item.num,
-//             text: item.name
-//           })
-//         );
-//       });
-//     }
-//   });
-// // END ONLINE DATA ACCESS
+  // // START ONLINE DATA ACCESS
+  //   $.ajax({
+  //     url: "/team/",
+  //     type: "GET",
+  //     dataType: "json",
+  //     success: data => {
+  //       console.log("You received some data!", data);
+  //       $.each(data, function(i, item) {
+  //         $("#numBox").append(
+  //           $("<option>", {
+  //             value: item.num,
+  //             text: item.num + ": " + item.name
+  //           })
+  //         );
+  //       });
+  //     }
+  //   });
+
+  //   $.ajax({
+  //     url: "/user/",
+  //     type: "GET",
+  //     dataType: "json",
+  //     success: data => {
+  //       console.log("You received some data!", data);
+  //       $.each(data, function(i, item) {
+  //         $("#nameBox").append(
+  //           $("<option>", {
+  //             value: item.num,
+  //             text: item.name
+  //           })
+  //         );
+  //       });
+  //     }
+  //   });
+  // // END ONLINE DATA ACCESS
 
   $("#createQr").click(e => {
     e.preventDefault();
@@ -83,6 +216,7 @@ loadLocalScouts();
       shooterDistanceValue += parseInt(this.value);
     });
 
+    $("#qrcode").html("");
     // generate qr code
     var qrcode = new QRCode(document.getElementById("qrcode"), {
       // scouting_type, teamNum, weight, trench, ball, shooter, notes, created_time, created_by, drive_train
@@ -106,6 +240,8 @@ loadLocalScouts();
       colorLight: "#ffffff",
       correctLevel: QRCode.CorrectLevel.H
     });
+
+    $("#clearFormButton").removeClass("invisible");
 
     //     // send data directly to server
     //     $.ajax({
@@ -138,6 +274,12 @@ loadLocalScouts();
     //     });
   });
 
+  $("#clearFormButton").click(e => {
+    e.preventDefault();
+    location.reload();
+    return false;
+  });
+  
   // define a generic Ajax error handler:
   // http://api.jquery.com/ajaxerror/
   $(document).ajaxError(() => {
@@ -146,7 +288,6 @@ loadLocalScouts();
 });
 
 function loadLocalScouts() {
-  //var data = JSON.parse(scoutNames);
   $.each(scoutNames, function(i, item) {
     $("#nameBox").append(
       $("<option>", {
@@ -155,4 +296,19 @@ function loadLocalScouts() {
       })
     );
   });
+}
+
+function loadLocalTeams() {
+  $.each(teamNames, function(i, item) {
+    $("#numBox").append(
+      $("<option>", {
+        value: item.num,
+        text: item.num + ": " + item.name
+      })
+    );
+  });
+}
+
+window.onbeforeunload = function () {
+  window.scrollTo(0, 0);
 }
